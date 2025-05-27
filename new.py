f89d0b1647e3bd142995be4baf0f4d603194a45e
redxander <redxander1030@gmail.com>
2025-05-27 12:58:23 -0230

    rand_letter

--- new.py
+++ new.py
@@ -192,3 +192,12 @@ for letc in letters:
         break
 
 print(rand_letter)
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("b is less than a")
